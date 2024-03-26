75343ab866aafb3837c60f27b3e8fdc4f887efac
German <germanlepin@gmail.com>
2024-03-26 16:12:17 +0100

    creating the scheme of the project

--- go.mod
+++
@@ -1,8 +0,0 @@
-module sso
-
-go 1.22.0
-
-require (
-	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.3.0 // indirect
-	google.golang.org/protobuf v1.28.1 // indirect
-)
---
+++ sso/internal/grpc/auth/server.go
@@ -0,0 +1,39 @@
+package auth
+
+import (
+	"context"
+	"errors"
+	ssov1 "https://github.com/GermanLepin/SSOprotos/gen/go/sso"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type serverAPI struct {
+	ssov1.UnimplementedAuthServer
+	auth Auth
+}
+
+func (s *serverAPI) Register(
+	ctx context.Context,
+	in *ssov1.RegisterRequest,
+) (*ssov1.RegisterResponse, error) {
+	if in.Email == "" {
+		return nil, status.Error(codes.InvalidArgument, "email is required")
+	}
+
+	if in.Password == "" {
+		return nil, status.Error(codes.InvalidArgument, "password is required")
+	}
+
+	uid, err := s.auth.RegisterNewUser(ctx, in.GetEmail(), in.GetPassword())
+	if err != nil {
+		if errors.Is(err, storage.ErrUserExists) {
+			return nil, status.Error(codes.AlreadyExists, "user already exists")
+		}
+
+		return nil, status.Error(codes.Internal, "failed to register user")
+	}
+
+	return &ssov1.RegisterResponse{UserId: uid}, nil
+}
